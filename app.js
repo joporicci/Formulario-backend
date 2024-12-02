af2ef2199d08975980834e6d7cf1229fe13a7789
joporicci <juanricci488@gmail.com>
2024-12-01 23:00:53 -0300

    Servidor completo

--- app.js
+++ app.js
@@ -73,6 +73,10 @@ app.use((err, req, res, next) => {
 // Configuración del puerto
 const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+    res.send('¡Hola desde Express desplegado en Vercel!');
+  });
+
 // Inicio del servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
