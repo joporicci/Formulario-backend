56224d3ef99eed627f725b78840157973a723e03
joporicci <juanricci488@gmail.com>
2024-12-03 18:33:21 -0300

    Same site none de nuevo

--- controllers/login.js
+++ controllers/login.js
@@ -47,7 +47,7 @@ export const loginUser = async (req, res) => {
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: 60 * 60 * 1000,
         path:'/'
       });
